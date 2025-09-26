7d49b73bd4dbd0b25b18f0476c5bb061936a9e98
Michael Sekhubede <43635891+sekhubede@users.noreply.github.com>
2025-09-26 11:57:33 +0000

    feat: updated Slim backend parameters to idicate intentional unuse

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,12 +1,12 @@
 <?php
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 // Simple test route
-$app->get('/', function ($request, $response) {
+$app->get('/', function ($_request, $response) {
     $response->getBody()->write("Hello, world!");
     return $response;
 });
